791b8272dd9c4a6db80c17dce390f467d2bc2b9c
root <1179916619@qq.com>
2025-03-18 03:02:16 +0000

    Initial commit

---
+++ sclient/client.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/pingcap-incubator/tinykv/kv/raftstore/scheduler_client"
+	"github.com/pingcap-incubator/tinykv/proto/pkg/metapb"
+)
+
+func main() {
+	schedulerClient, err := scheduler_client.NewClient(strings.Split("192.168.2.18:2379", ","), "")
+	if err != nil {
+		panic(err)
+	}
+	//check if   up
+	tem := 2
+	stores, err := schedulerClient.GetAllStores(context.TODO())
+	if err != nil {
+		log.Print(err.Error())
+	}
+	if stores == nil {
+		log.Print("stores not found")
+	}
+	storeLink := map[int]uint64{}
+	for _, store := range stores {
+		str := store.Address
+		re := regexp.MustCompile(`tinykv-(\d+)`)
+		matches := re.FindStringSubmatch(str)
+		if len(matches) >= 2 {
+			num, err := strconv.Atoi(matches[1])
+			if err != nil {
+				panic("转换失败: " + err.Error())
+			}
+			storeLink[num] = store.GetId()
+		}
+	}
+	st := storeLink[tem]
+	store, err := schedulerClient.GetStore(context.TODO(), st)
+	if err != nil {
+		log.Print(err.Error())
+	}
+	if store.GetState() == metapb.StoreState_Up {
+
+		schedulerClient.OfflineStore(context.TODO(), st)
+
+	} else if store.GetState() == metapb.StoreState_Tombstone {
+		schedulerClient.RemoveStore(context.TODO(), st)
+	}
+	println(store.GetState().String())
+	schedulerClient.Close()
+}
